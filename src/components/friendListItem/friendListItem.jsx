ab364fae90a9383adbdb0ed6f10989eaec8e06d3
Vladislav-Pavlenko <151663067+Vladislav-Pavlenko@users.noreply.github.com>
2024-06-12 15:04:41 +0300

    correct homework

--- src/components/friendListItem/friendListItem.jsx
+++ src/components/friendListItem/friendListItem.jsx
@@ -3,7 +3,9 @@ export default function FriendListItem({ avatar, name, isOnline }) {
     <div>
       <img src={avatar} alt="Avatar" width="48" />
       <p>{name}</p>
-      <p>{isOnline ? "Online" : "Offline"}</p>
+      <p className={isOnline ? "online" : "offline"}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
     </div>
   );
 }
